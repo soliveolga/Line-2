f9f9a8a95df4678d869c04b8988d28c1b40f6b20
BosykAnton <anton_bosyk@mail.ru>
2022-02-09 10:59:46 +0300

    задание 2 так же сдеала вывод на экран

---
+++ src/com/line/idea/Main.java
@@ -0,0 +1,11 @@
+package com.line.idea;
+
+public class Main {
+    public static void main(String[] args) {
+        double aaa = 3.17657;
+        byte bbb = 3;
+
+        System.out.println(aaa);
+        System.out.println(bbb);
+    }
+}
